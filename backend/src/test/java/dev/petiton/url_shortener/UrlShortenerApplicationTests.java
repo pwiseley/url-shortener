ddae7809d29e6472ebba8974030bf3e26df5f647
Petiton Wiseley Paul-Enzer <wppet@ulaval.ca>
2026-05-26 07:15:46 -0400

    fix: exclude DB configuration in tests to valid CI

--- backend/src/test/java/dev/petiton/url_shortener/UrlShortenerApplicationTests.java
+++ backend/src/test/java/dev/petiton/url_shortener/UrlShortenerApplicationTests.java
@@ -2,8 +2,12 @@ package dev.petiton.url_shortener;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+		"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+})
 class UrlShortenerApplicationTests {
 
 	@Test
